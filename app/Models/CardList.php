7aedf0bfececeed1177084554605c6226d075e5d
Omid <o.shabani@hotmail.com>
2022-03-05 23:58:21 +0330

    create CardList model / impelemtn relations

--- app/Models/CardList.php
+++ app/Models/CardList.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class CardList extends Model
 {
     use HasFactory;
+
+    public function board()
+    {
+        $this->belongsTo(Board::class,'board_id');
+    }
+
+    public function cards()
+    {
+        $this->hasMany(Card::class,'list_id');
+    }
 }
